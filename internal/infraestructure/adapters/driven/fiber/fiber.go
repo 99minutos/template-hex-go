049322f20aa56cbebfa7f1fbeda42719208566c4
Carlos Rafael Arreola Hernández <rafael-arreola@outlook.com>
2023-08-03 16:19:35 -0700

    add custom error middleware

--- internal/infraestructure/adapters/driven/fiber/fiber.go
+++ internal/infraestructure/adapters/driven/fiber/fiber.go
@@ -1,6 +1,7 @@
 package fiber
 
 import (
+	"errors"
 	"fmt"
 	"github.com/bytedance/sonic"
 	"github.com/gofiber/fiber/v2"
@@ -11,10 +12,22 @@ type FiberServer struct {
 	Server *fiber.App
 }
 
+type RestError struct {
+	Cause string `json:"cause"`
+}
+
 func NewFiberServer() *FiberServer {
 	server := fiber.New(fiber.Config{
 		JSONEncoder: sonic.Marshal,
 		JSONDecoder: sonic.Unmarshal,
+		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+			code := fiber.StatusInternalServerError
+			var e *fiber.Error
+			if errors.As(err, &e) {
+				code = e.Code
+			}
+			return ctx.Status(code).JSON(RestError{Cause: err.Error()})
+		},
 	})
 	server.Use(cors.New())
 	return &FiberServer{
